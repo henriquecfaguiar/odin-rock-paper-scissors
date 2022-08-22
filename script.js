432fdbda061db1098838f56bbc1d1a24823bf2cf
henriquecfaguiar <henriquecfaguiar@gmail.com>
2022-08-22 14:39:24 -0300

    Add function that plays one round

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const possibleChoices = ["Rock", "Paper", "Scissors"]
+const possibleChoices = ["rock", "paper", "scissors"]
 
 function getComputerChoice() {
-    const computerChoice = possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
-    console.log(computerChoice);
-    return computerChoice;
+    return possibleChoices[Math.floor(Math.random() * possibleChoices.length)];;
 }
+
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock")) {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
+        console.log(`You won! ${playerSelection} beats ${computerSelection}.`);
+    } else {
+        console.log("It's a tie!")
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(`Player's choice: ${playerSelection}\nComputer's choice: ${computerSelection}`)
+console.log(playRound(playerSelection.toLowerCase(), computerSelection));
