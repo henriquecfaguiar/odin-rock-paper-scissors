08857a4f20ce83271a548ce1d0688e3b513947da
henriquecfaguiar <henriquecfaguiar@gmail.com>
2022-08-22 15:00:12 -0300

    Update playRound func and get input from user

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const possibleChoices = ["rock", "paper", "scissors"]
+const possibleChoices = ["rock", "paper", "scissors"];
+const playerSelection = prompt("Choose rock, paper or scissors.");
+const computerSelection = getComputerChoice();
 
 function getComputerChoice() {
     return possibleChoices[Math.floor(Math.random() * possibleChoices.length)];;
@@ -9,13 +11,12 @@ function playRound(playerSelection, computerSelection) {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
     } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
         console.log(`You won! ${playerSelection} beats ${computerSelection}.`);
-    } else {
+    } else if (playerSelection === computerSelection) {
         console.log("It's a tie!")
+    } else {
+        console.log("Make sure to enter rock, paper or scissors.")
     }
 }
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
 
 console.log(`Player's choice: ${playerSelection}\nComputer's choice: ${computerSelection}`)
 console.log(playRound(playerSelection.toLowerCase(), computerSelection));
